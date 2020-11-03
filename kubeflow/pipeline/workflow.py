763db66ee7e198450e426e0cb1e3ef29f3c17f92
Rajendra Singh <rajendra.singh@oneconvergence.com>
2020-11-03 13:52:33 -0600

    test to make pipeline invalid

--- kubeflow/pipeline/workflow.py
+++ kubeflow/pipeline/workflow.py
@@ -22,7 +22,7 @@ component_store = kfp.components.ComponentStore(
 )
 
 # Create component factories
-add_op = component_store.load_component("my_add")
+add_op = component_store.load_component("my_add1")
 divide_op = component_store.load_component("my_divide")
 
 # Define pipeline
